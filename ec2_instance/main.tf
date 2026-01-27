7c8cef2962acddecedbd031738168221ca2b6ab4
social9009 <akshaysawant9009@gmail.com>
2026-01-27 13:36:03 +0530

    Update main.tf

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
   ami           = var.ami
   instance_type = var.instance_type
-  key_name      = "shell" 
+  key_name      = "mackbook-air-M1-2025" 
   tags = {
     Name = var.instance_name
   }
